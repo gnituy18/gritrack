ec06e5256b3e30be9fd9648d8d09adab976efe8e
gnituy18 <you@hsuyuting.com>
2022-09-18 21:16:15 +0800

    wip

--- web/src/lib/components/login/GoogleLoginButton.svelte
+++ web/src/lib/components/login/GoogleLoginButton.svelte
@@ -1,44 +1,48 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { env } from "$env/dynamic/public";
-  import Button from "$lib/components/common/Button.svelte";
-  import { goto } from "$app/navigation";
   import v1 from "$lib/apis/v1";
+  import { goto } from "$app/navigation";
 
-  function signIn() {
-    gapi.load("client:auth2", async () => {
-      try {
-        await gapi.client.init({
-          clientId: env.PUBLIC_GOOGLE_CLIENT_ID,
-          scope: "openid email profile",
-        });
-        const user = await gapi.auth2.getAuthInstance().signIn();
-        const accessToken = user.getAuthResponse().access_token;
-        const resp = await fetch(v1("/auth"), {
-          method: "POST",
-          redirect: "manual",
-          credentials: "include",
-          headers: {
-            "content-type": "application/json",
-            "Access-Control-Request-Method": "POST",
-            "Access-Control-Request-Headers": "Content-Type",
-          },
-          body: JSON.stringify({
-            type: 1,
-            google: { accessToken },
-          }),
-        });
-        if (resp.type === "opaqueredirect") {
-          await goto("/");
-        }
-      } catch (err) {
-        console.error(err);
+  async function handleCredentialResponse(response) {
+
+    try {
+      const resp = await fetch(v1("/auth"), {
+        method: "POST",
+        redirect: "manual",
+        credentials: "include",
+        headers: {
+          "content-type": "application/json",
+          "Access-Control-Request-Method": "POST",
+          "Access-Control-Request-Headers": "Content-Type",
+        },
+        body: JSON.stringify({
+          type: 1,
+          google: { accessToken: response.credential },
+        }),
+      });
+      if (resp.type === "opaqueredirect") {
+        await goto("/");
       }
-    });
+    } catch (e) {
+      console.error(e);
+    }
   }
+
+  onMount(() => {
+    google.accounts.id.initialize({
+      client_id: env.PUBLIC_GOOGLE_CLIENT_ID,
+      callback: handleCredentialResponse,
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("googleSignInButton"),
+      { theme: "outline", size: "large" } // customization attributes
+    );
+  });
 </script>
 
 <svelte:head>
-  <script async defer src="https://apis.google.com/js/api.js"></script>
+  <script src="https://accounts.google.com/gsi/client" async defer></script>
 </svelte:head>
 
-<Button onClick={signIn} value="Log in with Google" />
+<div id="googleSignInButton" />
