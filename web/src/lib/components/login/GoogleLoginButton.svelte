1712c55ff288608528fde4fa97d0304323340ace
gnituy18 <you@hsuyuting.com>
2022-09-21 22:19:00 +0800

    fix google types

--- web/src/lib/components/login/GoogleLoginButton.svelte
+++ web/src/lib/components/login/GoogleLoginButton.svelte
@@ -3,43 +3,40 @@
   import { env } from "$env/dynamic/public";
   import v1 from "$lib/apis/v1";
   import { goto } from "$app/navigation";
-  import * as google from "google-one-tap";
-
-  async function handleCredentialResponse(response) {
-    try {
-      const resp = await fetch(v1("/auth"), {
-        method: "POST",
-        redirect: "manual",
-        credentials: "include",
-        headers: {
-          "content-type": "application/json",
-          "Access-Control-Request-Method": "POST",
-          "Access-Control-Request-Headers": "Content-Type",
-        },
-        body: JSON.stringify({
-          type: 1,
-          google: { idToken: response.credential },
-        }),
-      });
-      if (resp.type === "opaqueredirect") {
-        await goto("/");
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  }
 
   onMount(() => {
     google.accounts.id.initialize({
       client_id: env.PUBLIC_GOOGLE_CLIENT_ID,
-      callback: handleCredentialResponse,
+      callback: async ({ credential }) => {
+        try {
+          const resp = await fetch(v1("/auth"), {
+            method: "POST",
+            redirect: "manual",
+            credentials: "include",
+            headers: {
+              "content-type": "application/json",
+              "Access-Control-Request-Method": "POST",
+              "Access-Control-Request-Headers": "Content-Type",
+            },
+            body: JSON.stringify({
+              type: 1,
+              google: { idToken: credential },
+            }),
+          });
+          if (resp.type === "opaqueredirect") {
+            await goto("/");
+          }
+        } catch (e) {
+          console.error(e);
+        }
+      },
     });
   });
 
   afterUpdate(() => {
     google.accounts.id.renderButton(
       document.getElementById("googleSignInButton"),
-      { theme: "outline", size: "large" }
+      { type: "standard", theme: "outline", size: "large" }
     );
   });
 </script>
