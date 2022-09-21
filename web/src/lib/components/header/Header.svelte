291f1b50cb23a7367d0dffa6e0fd31d1dc21ec19
許煜廷 <me@hsuyuting.com>
2022-09-22 02:24:45 +0800

    new header style

--- web/src/lib/components/header/Header.svelte
+++ web/src/lib/components/header/Header.svelte
@@ -5,6 +5,7 @@
   import Button from "$lib/components/common/Button.svelte";
   import Avatar from "$lib/components/common/Avatar.svelte";
   import Dropdown from "$lib/components/common/Dropdown.svelte";
+  import ItemPill from "$lib/components/header/ItemPill.svelte";
   import { goto } from "$app/navigation";
 
   async function deleteMission(missionId: string) {
@@ -18,42 +19,49 @@
   }
 </script>
 
-<header class="sticky top-0 flex-none box-border px-4 w-60 h-full border-r">
+<header
+  class="sticky top-0 flex flex-none flex-col justify-between box-border px-4 w-60 h-full border-r"
+>
   <nav>
-    <div class="flex justify-between items-center my-4">
+    <div class="m-4">
       <Avatar alt={$page.data.user.name} src={$page.data.user.picture} />
-      <Button
-        size="s"
-        theme="hidden"
-        value="Logout"
-        onClick={async () => {
-          await fetch(v1("/auth/logout"), {
-            method: "POST",
-            credentials: "include",
-          });
-
-          await goto("/");
-        }}
-      />
+      <h2 class="m-4 text-center">{$page.data.user.name}</h2>
     </div>
-    <div class="flex items-baseline">
-      <h2 class="mr-auto">Missions</h2>
-      <Button theme="hidden" size="s" href="/mission/create" value="Create" />
-    </div>
+    <hr />
+    <h2 class="m-4 text-center">Missions</h2>
     <ul class="my-4">
       {#each $missions as { id, name }}
-        <li class="mt-1 rounded px-2 hover:cursor-pointer hover:bg-slate-50 ">
+        <ItemPill>
           <a href="/{$page.data.user.id}/{name}" class="block text-lg"
             >{name}
             <Dropdown
-              classes="float-right"
+              classes="float-right invisible group-hover:visible"
               items={[{ label: "delete", action: () => deleteMission(id) }]}
             >
-              ...
+              ⋯
             </Dropdown>
           </a>
-        </li>
+        </ItemPill>
       {/each}
     </ul>
+    <a href="/mission/create">
+      <ItemPill classes="text-blue-400 text-center">New Mission</ItemPill>
+    </a>
   </nav>
+
+  <div class="text-center">
+    <Button
+      size="s"
+      theme="hidden"
+      value="Logout"
+      onClick={async () => {
+        await fetch(v1("/auth/logout"), {
+          method: "POST",
+          credentials: "include",
+        });
+
+        await goto("/");
+      }}
+    />
+  </div>
 </header>
