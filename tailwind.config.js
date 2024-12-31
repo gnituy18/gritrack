c9fd901d29aeee7c5f90da3a12f7b0218714dea8
許煜廷 <me@hsuyuting.com>
2024-12-31 12:29:30 +0800

    Update UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   content: ["./template/**/*.gotmpl", "./page/**/*.gotmpl"],
   theme: {
     extend: {
-      gridTemplateColumns: {
-        track: "minmax(0, 1fr) repeat(31, minmax(0, 20px))",
+      colors: {
+        eigengrau: {
+          50: "#f6f6f9",
+          100: "#ececf2",
+          200: "#d6d6e1",
+          300: "#b2b3c7",
+          400: "#888ba8",
+          500: "#696d8e",
+          600: "#545675",
+          700: "#444560",
+          800: "#3b3c51",
+          900: "#353545",
+          950: "#16161d"
+        }
       },
     },
   },
